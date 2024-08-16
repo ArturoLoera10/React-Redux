3553368887a14152dd4acf0c66048c272fb07da9
ArturoLoera10 <loeraarturo26@gmail.com>
2024-08-16 17:53:35 -0600

    List Todo with RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { PokemonApp } from "./PokemonApp.jsx";
+import { TodoApp } from "./TodoApp.jsx";
+// import { PokemonApp } from "./PokemonApp.jsx";
 // import App from "./App.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterSlice } from "./slices/counter";
 import { pokemonSlice } from "./slices/pokemon";
+import { todosApi } from "./apis";
 
 export const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
     pokemon: pokemonSlice.reducer,
+    [todosApi.reducerPath]: todosApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(todosApi.middleware),
 });
